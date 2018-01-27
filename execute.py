ddbd69e6001b74732fd18cbd09b2344625e0d3b2
Originneo <32263649+Originneo@users.noreply.github.com>
2018-01-27 10:32:57 +0300

    Update execute.py

--- execute.py
+++ execute.py
@@ -1,10 +1,21 @@
-
-def parse_token(token):
-    # ???
-    return token
-
-OPERATOR_TO_LAMBDA = {
-}
-
-def execute(line):
-    return 0.0
+import operator 
+ 
+def execute(expr):
+    OPERATOR_TO_LAMBDA = {'+': operator.add, '-': operator.sub, '*': operator.mul, '/': operator.truediv} 
+    stack = [0] 
+    for token in expr.split(" "): 
+        if token in OPERATOR_TO_LAMBDA: 
+            op2, op1 = stack.pop(), stack.pop() 
+            if token == '/':
+ 
+                 if isinstance(op1,int):
+                        stack.append(operator.floordiv(op1,int(op2)))
+                 else:
+                        stack.append(OPERATOR_TO_LAMBDA[token](op1,op2))
+            else:
+                 stack.append(OPERATOR_TO_LAMBDA[token](op1,op2))
+        elif token.isdigit():
+            stack.append(int(token))
+        else:
+            stack.append(float(token))
+    return stack.pop()
